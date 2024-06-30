0cf400c819b5296c9827c8bfdccd83dec9d98cb5
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-06-30 14:43:35 +0300

    Add MPA MVC Sample

---
+++ Lct05-AspNetCore-Introduction/MPA-ModelViewController/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MPA_ModelViewController.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Lct05-AspNetCore-Introduction/MPA-ModelViewController/Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MPA_ModelViewController.Models
+{
+    public class HomeViewModel
+    {
+        public DateTime UtcNow { get; init; }
+        public DateTime KyivNow { get; init; }
+        public DateTime TokyoNow { get; init; }
+    }
+}
