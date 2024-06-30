381a755d3ad53caa00ca1572d749fa0fd88072ee
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-06-30 15:01:03 +0300

    Add SPA Angular Sample

---
+++ Lct05-AspNetCore-Introduction/SPA-Angular/SPA-Angular.Server/Program.cs
@@ -0,0 +1,32 @@
+namespace SPA_Angular.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapFallbackToFile("/index.html");
+
+            app.Run();
+        }
+    }
+}
