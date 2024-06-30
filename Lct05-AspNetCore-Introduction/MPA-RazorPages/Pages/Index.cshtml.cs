fe201056f2d951c3fd25988ba0619eddb8f33208
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-06-30 14:40:02 +0300

    Add MPA Razor Pages Sample

---
+++ Lct05-AspNetCore-Introduction/MPA-RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MPA_RazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DateTime UtcNow { get; private set; }
+        public DateTime KyivNow { get; private set; }
+        public DateTime TokyoNow { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            UtcNow = DateTime.UtcNow;
+            KyivNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time"));
+            TokyoNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time"));
+        }
+    }
+}
