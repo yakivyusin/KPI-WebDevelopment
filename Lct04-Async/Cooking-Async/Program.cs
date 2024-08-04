845cee990ee65efdad0777005b02f29bb9850f7e
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-08-04 12:51:41 +0300

    Add Cooking Sync/Async Samples from MSDN

---
+++ Lct04-Async/Cooking-Async/Program.cs
@@ -0,0 +1,115 @@
+﻿// Source: https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
+
+namespace Cooking_Async;
+
+internal record Bacon;
+internal record Coffee;
+internal record Egg;
+internal record Juice;
+internal record Toast;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var cup = PourCoffee();
+        Console.WriteLine("coffee is ready");
+
+        var eggsTask = FryEggsAsync(2);
+        var baconTask = FryBaconAsync(3);
+        var toastTask = MakeToastWithButterAndJamAsync(2);
+
+        var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
+        while (breakfastTasks.Count > 0)
+        {
+            Task finishedTask = await Task.WhenAny(breakfastTasks);
+            if (finishedTask == eggsTask)
+            {
+                Console.WriteLine("eggs are ready");
+            }
+            else if (finishedTask == baconTask)
+            {
+                Console.WriteLine("bacon is ready");
+            }
+            else if (finishedTask == toastTask)
+            {
+                Console.WriteLine("toast is ready");
+            }
+            await finishedTask;
+            breakfastTasks.Remove(finishedTask);
+        }
+
+        var oj = PourOJ();
+        Console.WriteLine("oj is ready");
+        Console.WriteLine("Breakfast is ready!");
+    }
+
+    static async Task<Toast> MakeToastWithButterAndJamAsync(int number)
+    {
+        var toast = await ToastBreadAsync(number);
+        ApplyButter(toast);
+        ApplyJam(toast);
+
+        return toast;
+    }
+
+    private static Juice PourOJ()
+    {
+        Console.WriteLine("Pouring orange juice");
+        return new();
+    }
+
+    private static void ApplyJam(Toast toast) => Console.WriteLine("Putting jam on the toast");
+
+    private static void ApplyButter(Toast toast) => Console.WriteLine("Putting butter on the toast");
+
+    private static async Task<Toast> ToastBreadAsync(int slices)
+    {
+        for (int slice = 0; slice < slices; slice++)
+        {
+            Console.WriteLine("Putting a slice of bread in the toaster");
+        }
+
+        Console.WriteLine("Start toasting...");
+        await Task.Delay(3000);
+        Console.WriteLine("Remove toast from toaster");
+
+        return new();
+    }
+
+    private static async Task<Bacon> FryBaconAsync(int slices)
+    {
+        Console.WriteLine($"putting {slices} slices of bacon in the pan");
+        Console.WriteLine("cooking first side of bacon...");
+        await Task.Delay(3000);
+
+        for (int slice = 0; slice < slices; slice++)
+        {
+            Console.WriteLine("flipping a slice of bacon");
+        }
+
+        Console.WriteLine("cooking the second side of bacon...");
+        await Task.Delay(3000);
+        Console.WriteLine("Put bacon on plate");
+
+        return new();
+    }
+
+    private static async Task<Egg> FryEggsAsync(int howMany)
+    {
+        Console.WriteLine("Warming the egg pan...");
+        await Task.Delay(3000);
+        Console.WriteLine($"cracking {howMany} eggs");
+        Console.WriteLine("cooking the eggs ...");
+        await Task.Delay(3000);
+        Console.WriteLine("Put eggs on plate");
+
+        return new();
+    }
+
+    private static Coffee PourCoffee()
+    {
+        Console.WriteLine("Pouring coffee");
+        return new();
+    }
+}
---
+++ Lct04-Async/Cooking-Sync/Program.cs
@@ -0,0 +1,93 @@
+﻿// Source: https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
+
+namespace Cooking_Sync;
+
+internal record Bacon;
+internal record Coffee;
+internal record Egg;
+internal record Juice;
+internal record Toast;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var cup = PourCoffee();
+        Console.WriteLine("coffee is ready");
+
+        var eggs = FryEggs(2);
+        Console.WriteLine("eggs are ready");
+
+        var bacon = FryBacon(3);
+        Console.WriteLine("bacon is ready");
+
+        var toast = ToastBread(2);
+        ApplyButter(toast);
+        ApplyJam(toast);
+        Console.WriteLine("toast is ready");
+
+        var oj = PourOJ();
+        Console.WriteLine("oj is ready");
+        Console.WriteLine("Breakfast is ready!");
+    }
+
+    private static Juice PourOJ()
+    {
+        Console.WriteLine("Pouring orange juice");
+        return new();
+    }
+
+    private static void ApplyJam(Toast toast) => Console.WriteLine("Putting jam on the toast");
+
+    private static void ApplyButter(Toast toast) => Console.WriteLine("Putting butter on the toast");
+
+    private static Toast ToastBread(int slices)
+    {
+        for (int slice = 0; slice < slices; slice++)
+        {
+            Console.WriteLine("Putting a slice of bread in the toaster");
+        }
+
+        Console.WriteLine("Start toasting...");
+        Thread.Sleep(3000);
+        Console.WriteLine("Remove toast from toaster");
+
+        return new();
+    }
+
+    private static Bacon FryBacon(int slices)
+    {
+        Console.WriteLine($"putting {slices} slices of bacon in the pan");
+        Console.WriteLine("cooking first side of bacon...");
+        Thread.Sleep(3000);
+
+        for (int slice = 0; slice < slices; slice++)
+        {
+            Console.WriteLine("flipping a slice of bacon");
+        }
+
+        Console.WriteLine("cooking the second side of bacon...");
+        Thread.Sleep(3000);
+        Console.WriteLine("Put bacon on plate");
+
+        return new();
+    }
+
+    private static Egg FryEggs(int howMany)
+    {
+        Console.WriteLine("Warming the egg pan...");
+        Thread.Sleep(3000);
+        Console.WriteLine($"cracking {howMany} eggs");
+        Console.WriteLine("cooking the eggs ...");
+        Thread.Sleep(3000);
+        Console.WriteLine("Put eggs on plate");
+
+        return new();
+    }
+
+    private static Coffee PourCoffee()
+    {
+        Console.WriteLine("Pouring coffee");
+        return new();
+    }
+}
