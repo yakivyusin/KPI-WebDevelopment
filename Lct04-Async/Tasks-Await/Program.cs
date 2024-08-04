85786a145b743b3702cfbfa8991b194edbf40afd
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-08-04 15:01:37 +0300

    Add Tasks Samples

---
+++ Lct04-Async/Tasks-Await/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Http;
+
+namespace Tasks_Await;
+
+internal class Program
+{
+    private static readonly HttpClient _httpClient = new();
+    private static readonly IEnumerable<string> _urls =
+    [
+        "https://learn.microsoft.com",
+        "https://learn.microsoft.com/aspnet/core",
+        "https://learn.microsoft.com/azure",
+        "https://learn.microsoft.com/azure/devops",
+        "https://learn.microsoft.com/dotnet",
+        "https://learn.microsoft.com/dynamics365",
+        "https://learn.microsoft.com/education",
+        "https://learn.microsoft.com/enterprise-mobility-security",
+        "https://learn.microsoft.com/gaming",
+        "https://learn.microsoft.com/graph",
+        "https://learn.microsoft.com/microsoft-365",
+        "https://learn.microsoft.com/office",
+        "https://learn.microsoft.com/powershell",
+        "https://learn.microsoft.com/sql",
+        "https://learn.microsoft.com/surface",
+        "https://learn.microsoft.com/system-center",
+        "https://learn.microsoft.com/visualstudio",
+        "https://learn.microsoft.com/windows",
+        "https://learn.microsoft.com/maui"
+    ];
+
+    static async Task Main(string[] args)
+    {
+        var result = await SumPageSizesAsync();
+        Console.WriteLine($"Total size: {result}");
+    }
+
+    private static async Task<int> SumPageSizesAsync()
+    {
+        var total = 0;
+
+        foreach (string url in _urls)
+        {
+            Console.WriteLine(url);
+            total += await ProcessUrlAsync(url);
+        }
+
+        return total;
+    }
+
+    private static async Task<int> ProcessUrlAsync(string url)
+    {
+        var response = await _httpClient.GetAsync(url);
+        var content = await response.Content.ReadAsByteArrayAsync();
+
+        return content.Length;
+    }
+}
---
+++ Lct04-Async/Tasks-NoAwait/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Tasks_NoAwait;
+
+internal class Program
+{
+    private static readonly HttpClient _httpClient = new();
+    private static readonly IEnumerable<string> _urls =
+    [
+        "https://learn.microsoft.com",
+        "https://learn.microsoft.com/aspnet/core",
+        "https://learn.microsoft.com/azure",
+        "https://learn.microsoft.com/azure/devops",
+        "https://learn.microsoft.com/dotnet",
+        "https://learn.microsoft.com/dynamics365",
+        "https://learn.microsoft.com/education",
+        "https://learn.microsoft.com/enterprise-mobility-security",
+        "https://learn.microsoft.com/gaming",
+        "https://learn.microsoft.com/graph",
+        "https://learn.microsoft.com/microsoft-365",
+        "https://learn.microsoft.com/office",
+        "https://learn.microsoft.com/powershell",
+        "https://learn.microsoft.com/sql",
+        "https://learn.microsoft.com/surface",
+        "https://learn.microsoft.com/system-center",
+        "https://learn.microsoft.com/visualstudio",
+        "https://learn.microsoft.com/windows",
+        "https://learn.microsoft.com/maui"
+    ];
+
+#if true
+    static void Main(string[] args)
+    {
+        var t = SumPageSizesAsync();
+        t.Wait();
+
+        Console.WriteLine($"Total size: {t.Result}");
+    }
+#else
+    static Task Main(string[] args)
+    {
+        return SumPageSizesAsync().ContinueWith(t => Console.WriteLine($"Total size: {t.Result}"));
+    }
+#endif
+
+    private static Task<int> SumPageSizesAsync() => Task.WhenAll(_urls.Select(x => ProcessUrlAsync(x)))
+        .ContinueWith(t => t.Result.Sum());
+
+    private static Task<int> ProcessUrlAsync(string url)
+    {
+        Console.WriteLine(url);
+
+        return _httpClient.GetAsync(url)
+            .ContinueWith(t => t.Result.Content.ReadAsByteArrayAsync())
+            .Unwrap()
+            .ContinueWith(t => t.Result.Length);
+    }
+}
