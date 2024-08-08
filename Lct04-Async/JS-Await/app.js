6c47091dcea73c410abdb4be1b5c7efd155c1202
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-08-08 13:19:06 +0300

    Add JavaScript Promises and Await Samples

---
+++ Lct04-Async/JS-Await/app.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+var stdin = process.stdin;
+stdin.setRawMode(true);
+
+function fetchUrl(url) {
+    return fetch(url);
+}
+
+async function fetchUrlStatus(url) {
+    let response = await fetch(url);
+
+    return response.status;
+}
+
+function custom() {
+    return {
+        then: (onfulfilled, onrejected) => onfulfilled(1000)
+    };
+}
+
+(async () => {
+    let response = await fetchUrl('https://learn.microsoft.com');
+    console.log(response);
+
+    let status = await fetchUrlStatus('https://learn.microsoft.com/maui');
+    console.log(status);
+
+    let number = await custom();
+    console.log(number);
+})();
+
+stdin.resume();
+stdin.on('data', () => process.exit());
---
+++ Lct04-Async/JS-Promises/app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+let stdin = process.stdin;
+stdin.setRawMode(true);
+
+let fetchPromise = fetch('https://learn.microsoft.com').then(response => console.log(response));
+
+let customPromise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Hello World'), 2000);
+});
+
+customPromise.then((v) => console.log(`value: ${v}`), (v) => console.log(`error: ${v}`));
+
+Promise.resolve('Hello World 2').then((v) => console.log(`value: ${v}`), (v) => console.log(`error: ${v}`));
+
+stdin.resume();
+stdin.on('data', () => process.exit());
