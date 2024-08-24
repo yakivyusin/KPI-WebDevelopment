5ecaa09ee92d92d89091e1983571a04afc78484f
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-08-24 13:34:55 +0300

    Add Autofac Sample

---
+++ Lct08-AspNetCore-DependencyInjection/Autofac-WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Autofac.Features.Metadata;
+using Common_WeatherForecast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autofac_WebApp.Controllers;
+
+[ApiController]
+[Route("api/weather")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly Meta<IWeatherForecastStorage> _storage;
+
+    public WeatherForecastController(Meta<IWeatherForecastStorage> storage) => _storage = storage;
+
+    [HttpGet("meta")]
+    public object GetMetadata() => _storage.Metadata;
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get() => _storage.Value.GetAll();
+
+    [HttpGet("{date}")]
+    public WeatherForecast? Get(DateTime date) => _storage.Value.Get(DateOnly.FromDateTime(date));
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast forecast) => CreatedAtAction(nameof(Get), _storage.Value.Add(forecast));
+
+    [HttpDelete]
+    public void Delete() => _storage.Value.DeleteAll();
+
+    [HttpDelete("{date}")]
+    public void Delete(DateTime date) => _storage.Value.Delete(DateOnly.FromDateTime(date));
+}
---
+++ Lct08-AspNetCore-DependencyInjection/Autofac-WebApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Common_WeatherForecast;
+
+namespace Autofac_WebApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+        builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+        {
+            containerBuilder.RegisterType<WeatherForecastStorage>()
+                .As<IWeatherForecastStorage>()
+                .WithMetadata("Hello", "World")
+                .WithMetadata("Glory to", "Ukraine");
+        });
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
