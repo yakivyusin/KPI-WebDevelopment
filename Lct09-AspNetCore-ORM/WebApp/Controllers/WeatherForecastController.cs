c075e2a9b65b015334f9bd747f435b2e1c403b3e
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-08-25 15:02:06 +0300

    Add Web App + DbContext Sample

---
+++ Lct09-AspNetCore-ORM/WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using DataModel;
+using DataModel.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("api/weather")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly DataModelContext _context;
+
+    public WeatherForecastController(DataModelContext context) => _context = context;
+
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get() => await _context.Set<WeatherForecast>()
+        .ToListAsync();
+
+    [HttpGet("{date}")]
+    public async Task<WeatherForecast?> Get(DateTime date) => await _context.Set<WeatherForecast>()
+        .FindAsync(DateOnly.FromDateTime(date));
+
+    [HttpPost]
+    public async Task<IActionResult> Post(WeatherForecast forecast)
+    {
+        _context.Set<WeatherForecast>().Add(forecast);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Get), forecast);
+    }
+
+    [HttpDelete]
+    public async Task Delete() => await _context.Set<WeatherForecast>().ExecuteDeleteAsync();
+
+    [HttpDelete("{date}")]
+    public async Task Delete(DateTime date)
+    {
+        var entity = await _context.Set<WeatherForecast>()
+            .FindAsync(DateOnly.FromDateTime(date));
+
+        if (entity != null)
+        {
+            _context.Set<WeatherForecast>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Lct09-AspNetCore-ORM/WebApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<DataModelContext>(contextOptions =>
+            contextOptions.UseSqlite("Data Source=sample.db"));
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
