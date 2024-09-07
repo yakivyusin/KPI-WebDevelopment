e7989433a175a7b8f62f9a514e7784aefa2c4b0d
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-09-07 14:34:17 +0300

    Add IConfiguration Sample

---
+++ Lct11-AspNetCore-Configuration-Caching/IConfiguration/Controllers/ConfigurationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Configuration.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConfigurationController : ControllerBase
+{
+    private readonly IConfigurationRoot _configurationRoot;
+
+    public ConfigurationController(IConfiguration configuration) => _configurationRoot = (IConfigurationRoot)configuration;
+
+    [HttpGet("providers")]
+    public IEnumerable<string?> Providers() => _configurationRoot.Providers.Select(x => x.ToString());
+
+    [HttpGet("all")]
+    public IEnumerable<string> All() => _configurationRoot.AsEnumerable().Select(kv => kv.ToString());
+
+    [HttpGet("{key}")]
+    public string? Key(string key) => _configurationRoot[key];
+
+    [HttpGet("keyD")]
+    public int KeyD() => _configurationRoot.GetSection("MySection").GetSection("MySubsection").GetValue<int>("KeyD");
+}
---
+++ Lct11-AspNetCore-Configuration-Caching/IConfiguration/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Configuration;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddXmlFile("config.xml", optional: false, reloadOnChange: true);
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
