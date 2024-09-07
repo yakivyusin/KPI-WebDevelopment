afc3c27d3e2ece6641869c5f05e3e6f190309f1f
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-09-07 16:22:15 +0300

    Add Data Caching Sample

---
+++ Lct11-AspNetCore-Configuration-Caching/Caching-Data/Controllers/WeatherForecastController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text;
+using System.Text.Json;
+
+namespace Caching_Data.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly IMemoryCache _memoryCache;
+    private readonly IDistributedCache _distributedCache;
+
+    public WeatherForecastController(IMemoryCache memoryCache, IDistributedCache distributedCache)
+    {
+        _memoryCache = memoryCache;
+        _distributedCache = distributedCache;
+    }
+
+    [HttpGet("memory")]
+    public IEnumerable<WeatherForecast> GetMemory()
+    {
+#if true
+        var cachedData = _memoryCache.Get<IEnumerable<WeatherForecast>>(nameof(GetMemory));
+
+        if (cachedData != null)
+        {
+            return cachedData;
+        }
+        else
+        {
+            var data = WeatherForecast.GenerateRandom(10);
+            _memoryCache.Set(nameof(GetMemory), data, TimeSpan.FromSeconds(30));
+
+            return data;
+        }
+#else
+        return _memoryCache.GetOrCreate<IEnumerable<WeatherForecast>>(nameof(GetMemory), cacheEntry =>
+        {
+            cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30);
+
+            return WeatherForecast.GenerateRandom(10);
+        })!;
+#endif
+    }
+
+    [HttpGet("distributed")]
+    public async Task<IEnumerable<WeatherForecast>> GetDistributed()
+    {
+        var cachedData = await _distributedCache.GetAsync(nameof(GetDistributed));
+
+        if (cachedData != null)
+        {
+            return JsonSerializer.Deserialize<WeatherForecast[]>(Encoding.UTF8.GetString(cachedData))!;
+        }
+        else
+        {
+            var data = WeatherForecast.GenerateRandom(10);
+            _distributedCache.Set(nameof(GetDistributed), Encoding.UTF8.GetBytes(JsonSerializer.Serialize(data)), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30)
+            });
+
+            return data;
+        }
+    }
+}
---
+++ Lct11-AspNetCore-Configuration-Caching/Caching-Data/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Caching_Data;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddMemoryCache();
+        builder.Services.AddDistributedMemoryCache();
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
