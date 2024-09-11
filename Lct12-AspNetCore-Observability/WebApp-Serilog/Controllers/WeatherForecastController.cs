79157af99e6bdebd94f62a66959db198cddcf06b
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-09-11 10:35:54 +0300

    Add Serilog Sample

---
+++ Lct12-AspNetCore-Observability/WebApp-Serilog/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Serilog.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get(int page = 1, int pageSize = 10)
+    {
+        _logger.LogInformation("Requesting WeatherForecasts: Page {Page}, page size {PageSize}", page, pageSize);
+
+        return Enumerable.Range((page - 1) * pageSize + 1, pageSize).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ Lct12-AspNetCore-Observability/WebApp-Serilog/Program.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace WebApp_Serilog;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.ClearProviders();
+
+        builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        app.UseSerilogRequestLogging();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
