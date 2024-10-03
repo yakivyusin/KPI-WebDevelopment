dddb50f6d4182998b6457a93e1441ad422d32822
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-10-03 14:13:52 +0300

    Add HTML Forms Samples

---
+++ Lct14-Html/FormEchoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FormEchoApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        app.MapPost("/echo", async (HttpRequest request) =>
+        {
+            var form = await request.ReadFormAsync();
+
+            return string.Join(Environment.NewLine, form);
+        });
+
+        app.Run();
+    }
+}
