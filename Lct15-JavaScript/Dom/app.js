e2c73cad2247451ccac88a3416962ef103e8fb2c
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-10-19 12:16:55 +0300

    Add DOM Sample

---
+++ Lct15-JavaScript/Dom/app.js
@@ -0,0 +1,18 @@
+﻿console.log(`Title: ${document.title}, charset: ${document.characterSet}`);
+
+console.log(document.getElementsByTagName('body')[0]);
+
+let p = document.createElement('p');
+let attr = document.createAttribute('attr1');
+attr.value = 'value';
+p.setAttributeNode(attr);
+p.setAttribute('attr2', 'value');
+p.textContent = 'Lorem ipsum...';
+
+document.body.prepend(p);
+
+console.log(document.body.innerHTML);
+
+p.after(document.createComment('Oops...'));
+
+console.log(document.body.innerHTML);
