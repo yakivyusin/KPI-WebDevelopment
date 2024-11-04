a01084f58828a61e24b7cfae432fab6906c95a62
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-11-04 17:20:09 +0200

    Add TypeScript Sample

---
+++ Lct16-TypeScript/NodeConsoleApp/app.ts
@@ -0,0 +1,60 @@
+﻿import * as readline from 'node:readline/promises';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+type WeatherForecast = {
+    date: Date,
+    temperatureC: number,
+    temperatureF: number,
+    summary?: string
+};
+
+interface RequestData {
+    readonly start: number;
+    readonly count?: number;
+};
+
+class WeatherForecastService {
+    constructor(private url: string) {
+    }
+
+    async getWeatherForecast(requestData: RequestData): Promise<WeatherForecast[]> {
+        let path = this.url + `?start=${requestData.start}&count=${requestData.count ?? 5}`;
+        let response = await fetch(path, {
+            method: "GET"
+        });
+
+        return <WeatherForecast[]>(await response.json());
+    }
+}
+
+class App {
+    service = new WeatherForecastService("http://localhost:5122/weatherforecast");
+    
+    async run() {
+        while (true) {
+            let start = await rl.question("Start from: ");
+            let count = await rl.question("Count: ");
+
+            let data = await this.service.getWeatherForecast({
+                start: Number.parseInt(start),
+                count: Number.parseInt(count)
+            });
+
+            console.log(data);
+
+            let average = data.map(w => w.temperatureC).reduce((previous, current) => previous + current, 0) / data.length;
+
+            console.log(`Average TempC: ${average}`);
+        }
+    }
+}
+
+let app: App = new App();
+
+(async () => {
+    await app.run();
+})();
---
+++ Lct16-TypeScript/WebApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WebApplication;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (int start, int count) =>
+        {
+            var forecast = Enumerable.Range(start, count).Select(index =>
+                new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = summaries[Random.Shared.Next(summaries.Length)]
+                })
+                .ToArray();
+            return forecast;
+        });
+
+        app.Run();
+    }
+}
